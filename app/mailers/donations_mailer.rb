6e252e8c1c3b431f744b3fa46304ee2fbf43e3e0
Tanay Lakhani <tanayl@tanayl1.zillow.local>
2017-11-16 17:04:36 -0500

    test houndci

--- app/mailers/donations_mailer.rb
+++ app/mailers/donations_mailer.rb
@@ -4,11 +4,11 @@ class DonationsMailer < ApplicationMailer
 
     admins = donation.zone.admins
     reply_to = admins.map(&:email).reject(&:blank?).join(",")
-
+     ## testing hound
     mail(
-      to: donation.donor.email,
-      subject: t(".subject", range: @confirmation.time_range),
-      "Reply-To" => reply_to.presence || "info@freshfoodconnect.org",
-    )
+        to: "#{donation.donor.email}",
+        subject: t(".subject", range: @confirmation.time_range),
+        "Reply-To" => reply_to.presence || "info@freshfoodconnect.org",
+      )
   end
 end
